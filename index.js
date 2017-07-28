3e93cf43b87edf2d508e77d86b315d94c75fc58e
Diéffrei Tiepo de Quadros <dieffrei@gmail.com>
2017-07-28 23:22:18 +0200

    param support

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ const _ = require('underscore');
 const sleep = require('sleep');
 const fs = require('fs');
 const kale = require('kale');
+const restler = require('restler');
+const parseArgs = require('minimist');
+
+console.dir(parseArgs(process.argv));
+
+var args = parseArgs(process.argv);
+
+var codacyUser = args['codacy-user-name'];
+var repoName = args['repo-name'];
+var commitSha1 = args['commit-sha1'];
+var codacyProjectToken = args['codacy-project-token'];
+var userName = args['user-name'];
+var password = args['password'];
+var salesforceUrl = args['salesforce-url'];
 
 const conn = new jsforce.Connection({
-    loginUrl: 'https://test.salesforce.com'
+    loginUrl: salesforceUrl
 });
 
 const getConnection = async() => {
-    return await conn.login("dieffrei.quadros@eu-cosan.com.qa",
-        'Tobi86!@IXnHqS6Geypr502FEDt3nJGee');
+//    return await conn.login("dieffrei.quadros@eu-cosan.com.qa",
+//        'Tobi86!@IXnHqS6Geypr502FEDt3nJGee');
+    return await conn.login(userName, password);
 };
 
 const findAllTestClassesIds = async() => {
@@ -31,31 +46,29 @@ const queryTestExecutionStatus = async(testJobId) => {
 };
 
 /* CODACY CODE COVERAGE FORMAT
-{
-    "total": 23,
-    "fileReports": [
-    {
-        "filename": "src/Codacy/Coverage/Parser/CloverParser.php",
-        "total": 54,
-        "coverage": {
-            "3": 3,
-            "7": 1
-        }
-    }
-]
-}
-*/
-function toCodacyFormat(salesforceCodeCoverageResult) {
+ {
+ "total": 23,
+ "fileReports": [
+ {
+ "filename": "src/Codacy/Coverage/Parser/CloverParser.php",
+ "total": 54,
+ "coverage": {
+ "3": 3,
+ "7": 1
+ }
+ }
+ ]
+ }
+ */
+function toCodacyFormat(salesforceCodeCoverageResult, wideCodeCoverage) {
 
     var codacyFormatData = {
-        total: 100,
+        total: wideCodeCoverage.PercentCovered,
         fileReports: []
     };
 
     _.each(salesforceCodeCoverageResult, (classCoverage) => {
-        
-        console.log(classCoverage.ApexClassOrTrigger);
-        
+
         var codacyFormatDataItem = {
             filename: classCoverage.ApexClassOrTrigger.Name,
             total: classCoverage.NumLinesCovered / classCoverage.NumLinesUncovered,
@@ -69,32 +82,47 @@ function toCodacyFormat(salesforceCodeCoverageResult) {
         }
 
         codacyFormatData.fileReports.push(codacyFormatDataItem);
-        
+
     });
 
     return codacyFormatData;
 }
 
 const calculateCodeCoverage = async() => {
-    
+
     await getConnection();
+
     const testClasses = await findAllTestClassesIds();
     const testClassIds = _.pluck(testClasses, 'Id');
     console.log('Test classes ' + testClassIds);
-    const jobId  = await conn.tooling.runTestsAsynchronous(testClassIds.slice(0,1));
+
+    const jobId = await conn.tooling.runTestsAsynchronous(testClassIds.slice(0, 1));
     console.log('Job Id [' + jobId + ']');
-    
-    while(!await queryTestExecutionStatus(jobId)){
+
+    while (!await queryTestExecutionStatus(jobId)) {
         console.log('Testing is running');
         sleep.sleep(1);
     }
 
     const codeCoverageResults = await conn.tooling.sobject('ApexCodeCoverageAggregate')
-        .find({}, 'Id, ApexClassOrTrigger.Name, ApexClassOrTriggerId, NumLinesCovered, NumLinesUncovered, Coverage');
+        .find({}, 'Id, ApexClassOrTrigger.Name, ApexClassOrTriggerId, ' +
+            'NumLinesCovered, NumLinesUncovered, Coverage');
 
-    const json = toCodacyFormat(codeCoverageResults);
+    const wideCodeCoverage = await conn.tooling.sobject('ApexOrgWideCoverage').find({});
+    console.log(wideCodeCoverage[0]);
 
-    await fs.writeFile('code-coverage.json', JSON.stringify(json));
+    const json = toCodacyFormat(codeCoverageResults, wideCodeCoverage[0]);
+
+    rest.post("https://api.codacy.com/2.0/"
+        + codacyUser + "/" + repoName
+        + "/commit/" + commitSha1 + "/coverage/apex", {
+        headers: {
+            project_token: codacyProjectToken
+        },
+        data: json
+    }).on('complete', function (data, response) {
+        console.log(data, response);
+    });
 
 };
 
